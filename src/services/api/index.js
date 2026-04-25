c6a51721640a0cea66796ac79c3d9d7dfeda7b5c
anokhigupta-tech1 <anokhig174@gmail.com>
2026-04-25 17:44:24 +0530

    project files

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,56 @@
+// import axios from "axios";
+
+//  const API = axios.create({
+//   baseURL: import.meta.env.VITE_APP_API_URL,
+// });
+
+// // Request Interceptor
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token && config.headers) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// // Response Interceptor
+// API.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem("token");
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// // APIs
+// export const createAccount = (userCredentials) =>
+//   API.post("/auth/register", userCredentials);
+
+// export const loginUser = (userCredentials) =>
+//   API.post("/auth/login", userCredentials);
+
+// export const createProfessionalService = (data) =>
+//   API.post("/auth/register-profetionals", data);
+
+// export const getAllProfessionals = () =>
+//   API.get("/professional");
+
+// export const getProfessionalById = (id) =>
+//   API.get(`/professional/${id}`);
+
+// export default API;
+
 import axios from "axios";
 
- const API = axios.create({
+const API = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
@@ -15,7 +65,7 @@ API.interceptors.request.use(
 
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 // Response Interceptor
@@ -27,7 +77,7 @@ API.interceptors.response.use(
       window.location.href = "/login";
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 // APIs
@@ -40,10 +90,15 @@ export const loginUser = (userCredentials) =>
 export const createProfessionalService = (data) =>
   API.post("/auth/register-profetionals", data);
 
-export const getAllProfessionals = () =>
-  API.get("/professional");
+export const getAllProfessionals = () => API.get("/professional");
 
-export const getProfessionalById = (id) =>
-  API.get(`/professional/${id}`);
+export const getProfessionalById = (id) => API.get(`/professional/${id}`);
+
+export const updateProfileApi = (formData) =>
+  API.put("/user/update-profile", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
 export default API;
